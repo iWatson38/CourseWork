ece5df7230dc44c5fa3a52894951e9f44d1b0b31
Ivan Ivachin <Ivaschin5599@gmail.com>
2022-04-19 16:40:51 +0700

    feat: StarCard component

---
+++ components/MainView/GiftsForStars/StarCard/StarCard.component.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+    ButtonComponent,
+    EButtonStyleType,
+} from 'components/UI/Button/Button.component';
+import { useRouter } from 'next/router';
+import SGoodCardComponent from 'components/Cards/GoodCard/GoodCard.module.scss';
+import SCategoryCardComponent from 'components/Catalog/Categories/CategoryCardComponent/CategoryCard.module.scss';
+import SStarCardComponent from './StarCard.module.scss';
+
+export interface ICategoryCardComponentProps {
+    starId: number;
+    starName: string;
+    img: string;
+    className?: string;
+}
+
+export const StarCardComponent: React.FC<ICategoryCardComponentProps> = ({
+    starId,
+    starName,
+    img,
+    className,
+}) => {
+    const router = useRouter();
+    const groupedGiftsRedirect = () => {
+        router.push(`/friends/${starId}`);
+    };
+
+    return (
+        <div className={[className, SStarCardComponent.Card].join(' ')}>
+            <img src={img} alt="group" className={SGoodCardComponent.Image} />
+            <p className={SStarCardComponent.Title}>{starName}</p>
+            <ButtonComponent
+                className={SStarCardComponent.Button}
+                onClick={groupedGiftsRedirect}
+            >
+                Посмотреть
+            </ButtonComponent>
+            <ButtonComponent
+                className={SCategoryCardComponent.MobileButton}
+                styleType={EButtonStyleType.WHITE}
+                onClick={groupedGiftsRedirect}
+            >
+                Посмотерть
+            </ButtonComponent>
+        </div>
+    );
+};
