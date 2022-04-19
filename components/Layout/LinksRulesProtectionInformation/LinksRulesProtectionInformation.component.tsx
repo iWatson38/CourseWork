f6ab2a7beae40cfc4d85cbd33bf73da24db69eb3
Ivan Ivachin <Ivaschin5599@gmail.com>
2022-04-19 16:37:14 +0700

    feat: LinksRulesProtectionInformation component

---
+++ components/Layout/LinksRulesProtectionInformation/LinksRulesProtectionInformation.component.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import SCommon from 'styles/Common.module.scss';
+import SLinksRulesProtectionInformation from './LinksRulesProtectionInformation.module.scss';
+
+interface ILinksRulesProtectionInformationProps {
+    className?: string;
+}
+
+export const LinksRulesProtectionInformation: React.FC<ILinksRulesProtectionInformationProps> =
+    ({ className }) => {
+        return (
+            <ul
+                className={[
+                    className,
+                    SLinksRulesProtectionInformation.InformationForUser,
+                    SCommon.Container,
+                ].join(' ')}
+            >
+                <li className={SLinksRulesProtectionInformation.LeftGridArea}>
+                    <a
+                        className={SLinksRulesProtectionInformation.LinkForUser}
+                        href="/privacy"
+                    >
+                        <span
+                            className={
+                                SLinksRulesProtectionInformation.LinkUnderLine
+                            }
+                        >
+                            Правила защиты информации о пользователях сайта
+                        </span>
+                    </a>
+                </li>
+
+                <li
+                    className={[
+                        SLinksRulesProtectionInformation.CopyRight,
+                        SLinksRulesProtectionInformation.CenterGridArea,
+                    ].join(' ')}
+                >
+                    Все права защищены 2021
+                </li>
+                <li className={SLinksRulesProtectionInformation.RightGridArea}>
+                    <a
+                        className={SLinksRulesProtectionInformation.LinkForUser}
+                        href="/terms"
+                    >
+                        <span
+                            className={
+                                SLinksRulesProtectionInformation.LinkUnderLine
+                            }
+                        >
+                            Пользователькое соглашение
+                        </span>
+                    </a>
+                </li>
+            </ul>
+        );
+    };
