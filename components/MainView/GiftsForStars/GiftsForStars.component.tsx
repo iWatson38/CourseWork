878c7bb82020ef08cb7d512ebb8c9cb5d0139f04
Ivan Ivachin <Ivaschin5599@gmail.com>
2022-04-19 16:40:18 +0700

    feat: GiftsForStars component

---
+++ components/MainView/GiftsForStars/GiftsForStars.component.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import SGiftsForStarsComponent from './GiftsForStars.module.scss';
+import { StarCardComponent } from './StarCard/StarCard.component';
+
+const Gift = 'GiftsForStars/Gift.svg';
+const Timothy = 'GiftsForStars/Timothy.svg';
+const Sobchak = 'GiftsForStars/Sobchak.svg';
+const Instosamka = 'GiftsForStars/Instosamka.svg';
+
+export const GiftsForStarsComponent: React.FC = () => {
+    return (
+        <div className={[SGiftsForStarsComponent.GiftsForStar].join(' ')}>
+            <div className={SGiftsForStarsComponent.TitleContainer}>
+                <img src={Gift} alt="gift" />
+                <p className={SGiftsForStarsComponent.Title}>
+                    Посмотри, что шаман подобрал для звёзд:
+                </p>
+            </div>
+
+            <ul className={SGiftsForStarsComponent.StarList}>
+                <li key={11547416}>
+                    <StarCardComponent
+                        starId={11547416}
+                        starName="Тимати"
+                        img={Timothy}
+                    />
+                </li>
+                <li key={399053643}>
+                    <StarCardComponent
+                        starId={399053643}
+                        starName="Инстасамкa"
+                        img={Instosamka}
+                    />
+                </li>
+                <li key={453382669}>
+                    <StarCardComponent
+                        starId={453382669}
+                        starName="Ксения Собчак"
+                        img={Sobchak}
+                    />
+                </li>
+            </ul>
+        </div>
+    );
+};
