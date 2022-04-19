0094b5d90a30e3e177520b44db7536b69896c26e
Ivan Ivachin <Ivaschin5599@gmail.com>
2022-04-19 16:21:41 +0700

    feat: Button component

---
+++ components/UI/Button/Button.component.tsx
@@ -0,0 +1,37 @@
+import { ButtonHTMLAttributes } from 'react';
+import SButtonComponent from './Button.module.scss';
+
+export enum EButtonStyleType {
+    OUTLINED = 'outlined',
+    WHITE = 'white',
+}
+
+interface IButtonComponentProps {
+    className?: string;
+    type?: ButtonHTMLAttributes<HTMLButtonElement>['type'];
+    styleType?: EButtonStyleType;
+    onClick?: () => void;
+}
+
+export const ButtonComponent: React.FC<IButtonComponentProps> = ({
+    className,
+    children,
+    type,
+    styleType,
+    onClick,
+}) => (
+    <button
+        onClick={onClick}
+        className={[
+            className,
+            SButtonComponent.Button,
+            styleType === EButtonStyleType.OUTLINED &&
+                SButtonComponent.Button_Outlined,
+            styleType === EButtonStyleType.WHITE &&
+                SButtonComponent.Button_White,
+        ].join(' ')}
+        type={type}
+    >
+        {children}
+    </button>
+);
