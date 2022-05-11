ddc3355b25ae8a734c8c999d7f981f708931753b
Ivan Ivachin <Ivaschin5599@gmail.com>
2022-05-11 17:01:06 +0700

    fix: code review

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const axios = require('axios');
+
 const siteUrl = process.env.NEXT_PUBLIC_APP_URL;
+const friendsIdUrl = process.env.NEXT_PUBLIC_FRIENDS_ID_API_URL;
 
 module.exports = {
     siteUrl,
@@ -10,10 +15,16 @@ module.exports = {
             { userAgent: '*', disallow: '/friends' },
             { userAgent: '*', allow: '/' },
         ],
-        additionalSitemaps: [
-            `${siteUrl}/sitemap.xml`,
-            `${siteUrl}/server-sitemap.xml`,
-        ],
+    },
+    additionalPaths: async (config) => {
+        const { data: friendsIdList } = await axios.get(
+            `${friendsIdUrl}/api/v2/users`,
+        );
+        const fields = friendsIdList.data.map((friendId) => ({
+            loc: `${siteUrl}/catalog/${friendId}`,
+            lastmod: new Date().toISOString(),
+        }));
+        return fields;
     },
     exclude: ['/signIn', '/favorites', '/friends'],
 };
