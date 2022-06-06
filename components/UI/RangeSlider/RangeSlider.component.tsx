f8615b849d310928ae8135fb4d14e412dc65cd25
Ivan Ivachin <Ivaschin5599@gmail.com>
2022-06-07 03:58:34 +0700

    feat: inputs for range filter

--- components/UI/RangeSlider/RangeSlider.component.tsx
+++ components/UI/RangeSlider/RangeSlider.component.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SRangeSliderComponent from './RangeSlider.module.scss';
 import { LRangeSliderComponent } from './RangeSlider.logic';
+import { InputComponent } from '../Input/Input.component';
 
 export interface IRangeSliderProps {
     min: number;
@@ -17,6 +18,11 @@ export const RangeSliderComponent = React.forwardRef<
     IRangeSliderRef,
     IRangeSliderProps
 >(({ min, max, currency, onChange }, ref) => {
+    const [rangeInPercent, setRangeInPercent] = useState({
+        min: min.toString(),
+        max: max.toString(),
+    });
+
     const {
         handleStartMove,
         leftDvider,
@@ -25,65 +31,136 @@ export const RangeSliderComponent = React.forwardRef<
         rightKnob,
         track,
         valueInPercent,
+        leftKnobMoving,
+        rightKnobMoving,
     } = LRangeSliderComponent({
         min,
         max,
-        onChange,
         ref,
+        minFromInput: Number(rangeInPercent.min),
+        maxFromInput: Number(rangeInPercent.max),
     });
 
+    useEffect(() => {
+        if (leftKnobMoving) {
+            setRangeInPercent({
+                min: Math.round(
+                    ((max - min) / 100) * valueInPercent[0] + min,
+                ).toString(),
+                max: rangeInPercent.max,
+            });
+        }
+        if (rightKnobMoving) {
+            setRangeInPercent({
+                min: rangeInPercent.min,
+                max: Math.round(
+                    ((max - min) / 100) * valueInPercent[1] + min,
+                ).toString(),
+            });
+        }
+    }, [valueInPercent, leftKnobMoving, rightKnobMoving]);
+
+    useEffect(() => {
+        if (onChange) {
+            onChange([
+                rangeInPercent.min.replace(/\s/g, ''),
+                rangeInPercent.max.replace(/\s/g, ''),
+            ]);
+        }
+    }, [rangeInPercent]);
+
     return (
-        <div ref={track} className={SRangeSliderComponent.RangeSlider}>
-            <div
-                tabIndex={0}
-                ref={leftKnob}
-                aria-valuenow={valueInPercent[0]}
-                role="slider"
-                onMouseDown={(e) => handleStartMove(e, 'left')}
-                onTouchMove={(e) => handleStartMove(e, 'left')}
-                className={SRangeSliderComponent.Knob}
-            >
-                <span
-                    className={[
-                        SRangeSliderComponent.KnobValue,
-                        SRangeSliderComponent.MinKnobValue,
-                    ].join(' ')}
-                >
-                    {Math.round(
-                        ((max - min) / 100) * valueInPercent[0] + min,
-                    ).toLocaleString('ru-Ru')}
-                    {currency}
-                </span>
-            </div>
-            <div ref={leftDvider} className={SRangeSliderComponent.Dvider} />
+        <>
+            <div ref={track} className={SRangeSliderComponent.RangeSlider}>
+                <div
+                    tabIndex={0}
+                    ref={leftKnob}
+                    aria-valuenow={valueInPercent[0]}
+                    role="slider"
+                    onMouseDown={(e) => handleStartMove(e, 'left')}
+                    onTouchMove={(e) => handleStartMove(e, 'left')}
+                    className={SRangeSliderComponent.Knob}
+                ></div>
+                <div
+                    ref={leftDvider}
+                    className={SRangeSliderComponent.Dvider}
+                />
 
-            <div className={SRangeSliderComponent.ProgressLine} />
+                <div className={SRangeSliderComponent.ProgressLine} />
 
-            <div
-                tabIndex={0}
-                ref={rightKnob}
-                aria-valuenow={valueInPercent[1]}
-                role="slider"
-                onMouseDown={(e) => handleStartMove(e, 'right')}
-                onTouchMove={(e) => handleStartMove(e, 'right')}
-                className={[
-                    SRangeSliderComponent.Knob,
-                    SRangeSliderComponent.KnobRight,
-                ].join(' ')}
-            >
-                <span
+                <div
+                    tabIndex={0}
+                    ref={rightKnob}
+                    aria-valuenow={valueInPercent[1]}
+                    role="slider"
+                    onMouseDown={(e) => handleStartMove(e, 'right')}
+                    onTouchMove={(e) => handleStartMove(e, 'right')}
                     className={[
-                        SRangeSliderComponent.KnobValue,
-                        SRangeSliderComponent.MaxKnobValue,
+                        SRangeSliderComponent.Knob,
+                        SRangeSliderComponent.KnobRight,
                     ].join(' ')}
-                >
-                    {Math.round(
-                        ((max - min) / 100) * valueInPercent[1] + min,
-                    ).toLocaleString('ru-Ru')}
-                    {currency}
-                </span>
+                ></div>
+                <div
+                    ref={rightDvider}
+                    className={SRangeSliderComponent.Dvider}
+                />
             </div>
-            <div ref={rightDvider} className={SRangeSliderComponent.Dvider} />
-        </div>
+            <div className={SRangeSliderComponent.InputsArea}>
+                <div className={SRangeSliderComponent.InputContainer}>
+                    От:
+                    <InputComponent
+                        type="number"
+                        styleType="gray"
+                        className={SRangeSliderComponent.RangeInput}
+                        value={rangeInPercent.min}
+                        onChange={(event) => {
+                            if (
+                                Number(event.target.value) <=
+                                    Number(rangeInPercent.max) &&
+                                Number(event.target.value) >= 0
+                            ) {
+                                setRangeInPercent({
+                                    min: event.target.value,
+                                    max: rangeInPercent.max,
+                                });
+                            }
+                        }}
+                    />
+                </div>
+                <div className={SRangeSliderComponent.InputContainer}>
+                    До:
+                    <InputComponent
+                        type="number"
+                        styleType="gray"
+                        className={SRangeSliderComponent.RangeInput}
+                        value={rangeInPercent.max}
+                        onBlur={(event) => {
+                            if (
+                                Number(event.target.value) <
+                                Number(rangeInPercent.min)
+                            ) {
+                                setRangeInPercent({
+                                    min: rangeInPercent.min,
+                                    max: rangeInPercent.min,
+                                });
+                            }
+                        }}
+                        onChange={(event) => {
+                            if (Number(event.target.value) <= max) {
+                                setRangeInPercent({
+                                    min: rangeInPercent.min,
+                                    max: event.target.value,
+                                });
+                            } else if (Number(event.target.value) > max) {
+                                setRangeInPercent({
+                                    min: rangeInPercent.min,
+                                    max: max.toString(),
+                                });
+                            }
+                        }}
+                    />
+                </div>
+            </div>
+        </>
     );
 });
--- components/UI/RangeSlider/RangeSlider.logic.ts
+++ components/UI/RangeSlider/RangeSlider.logic.ts
@@ -2,17 +2,19 @@ import React, { useEffect, useImperativeHandle, useRef, useState } from 'react';
 import { IRangeSliderRef } from './RangeSlider.component';
 
 interface ILRangeSliderComponentProps {
-    onChange?: (newValue: [string, string]) => void;
     min: number;
     max: number;
     ref: React.ForwardedRef<IRangeSliderRef>;
+    minFromInput: number;
+    maxFromInput: number;
 }
 
 export const LRangeSliderComponent = ({
     max,
     min,
-    onChange,
     ref,
+    minFromInput,
+    maxFromInput,
 }: ILRangeSliderComponentProps) => {
     const [leftKnobMoving, setLeftKnobMoving] = useState(false);
     const [rightKnobMoving, setRightKnobMoving] = useState(false);
@@ -40,9 +42,6 @@ export const LRangeSliderComponent = ({
                 rightDvider.current.style.width = '0px';
             }
             setValueInPercent([0, 100]);
-            if (onChange) {
-                onChange([`${min || 0}`, `${max || 20000}`]);
-            }
         },
     }));
 
@@ -68,19 +67,72 @@ export const LRangeSliderComponent = ({
         setRightKnobMoving(false);
     };
 
-    const handleOnChange = (percent: [number, number]) => {
-        const changeValue: [string, string] = [
-            Math.round(((max - min) / 100) * percent[0] + min).toLocaleString(
-                'ru-Ru',
-            ),
-            Math.round(((max - min) / 100) * percent[1] + min).toLocaleString(
-                'ru-Ru',
-            ),
-        ];
-        if (onChange) {
-            onChange(changeValue);
+    useEffect(() => {
+        if (
+            leftKnob.current &&
+            rightKnob.current &&
+            leftDvider.current &&
+            track.current
+        ) {
+            const trackWidth = track.current.clientWidth;
+            const leftKnobWidth = leftKnob.current.clientWidth;
+            const leftKnobPercentSize =
+                ((leftKnobWidth + 4) * 100) / trackWidth;
+            const rightKnobPercent = Number(
+                rightKnob.current.style.right.replace('%', ''),
+            );
+            const leftKnobPercent = (minFromInput * 100) / max;
+
+            if (leftKnobPercent <= 100 - rightKnobPercent) {
+                const leftKnobPercentValue =
+                    leftKnobPercent > 92
+                        ? leftKnobPercent -
+                          leftKnobPercentSize -
+                          rightKnobPercent
+                        : leftKnobPercent;
+                console.log(leftKnobPercentValue);
+                leftKnob.current.style.left = `${leftKnobPercentValue}%`;
+                leftKnob.current.style.zIndex = '10';
+                rightKnob.current.style.zIndex = '9';
+                leftDvider.current.style.width = `${
+                    (trackWidth * leftKnobPercent) / 100
+                }px`;
+            }
         }
-    };
+    }, [minFromInput]);
+
+    useEffect(() => {
+        if (
+            rightKnob.current &&
+            leftKnob.current &&
+            rightDvider.current &&
+            track.current
+        ) {
+            const trackWidth = track.current.clientWidth;
+            const rightKnobWidth = rightKnob.current.clientWidth;
+            const rightKnobPercent = 100 - (maxFromInput * 100) / max;
+            const dviderWidth =
+                (track.current.clientWidth / 100) * rightKnobPercent;
+            const rightKnobPercentSize =
+                ((rightKnobWidth + 4) * 100) / trackWidth;
+
+            if (minFromInput <= maxFromInput) {
+                if (rightKnobPercent < rightKnobPercentSize) {
+                    leftKnob.current.style.zIndex = '9';
+                    rightKnob.current.style.zIndex = '10';
+                    rightKnob.current.style.right = `${rightKnobPercent}%`;
+                    rightDvider.current.style.width = `${dviderWidth}px`;
+                } else {
+                    leftKnob.current.style.zIndex = '9';
+                    rightKnob.current.style.zIndex = '10';
+                    rightKnob.current.style.right = `${
+                        rightKnobPercent - rightKnobPercentSize
+                    }%`;
+                    rightDvider.current.style.width = `${dviderWidth}px`;
+                }
+            }
+        }
+    }, [maxFromInput]);
 
     // EFFECTS
     useEffect(() => {
@@ -120,57 +172,35 @@ export const LRangeSliderComponent = ({
                         ((clientX - trackOffsetLeft - leftKnobWidth / 2) *
                             100) /
                         trackWidth;
-
                     if (
-                        percentValue + leftKnobPercentSize <= 100 &&
+                        percentValue + rightKnobPercentSize <=
+                            100 - rightKnobPercent &&
                         percentValue >= 0
                     ) {
-                        if (
-                            percentValue + leftKnobPercentSize <=
-                                100 - rightKnobPercent ||
-                            percentValue <= leftKnobPercent
-                        ) {
-                            leftKnob.current.style.left = `${percentValue}%`;
-                            setValueInPercent((prev) => [
-                                percentValue,
-                                prev[1],
-                            ]);
+                        leftKnob.current.style.zIndex = '10';
+                        rightKnob.current.style.zIndex = '9';
+                        leftKnob.current.style.left = `${percentValue}%`;
+                        setValueInPercent((prev) => [percentValue, prev[1]]);
 
-                            handleOnChange([percentValue, valueInPercent[1]]);
-
-                            leftDvider.current.style.width = `${
-                                clientX - trackOffsetLeft
-                            }px`;
-                        } else {
-                            leftKnob.current.style.left = `${
-                                100 - rightKnobPercent - leftKnobPercentSize
-                            }%`;
-                            setValueInPercent((prev) => [
-                                100 - rightKnobPercent + leftKnobPercentSize,
-                                prev[1],
-                            ]);
-
-                            handleOnChange([
-                                100 - rightKnobPercent + leftKnobPercentSize,
-                                valueInPercent[1],
-                            ]);
-
-                            leftDvider.current.style.width = `${trackWidth}px`;
-                        }
+                        leftDvider.current.style.width = `${
+                            clientX - trackOffsetLeft
+                        }px`;
                     } else if (
-                        percentValue + leftKnobPercentSize <=
+                        percentValue + leftKnobPercentSize >=
                         100 - rightKnobPercent
                     ) {
                         leftKnob.current.style.left = `${
-                            percentValue <= 0 ? 0 : 100 - leftKnobPercentSize
+                            percentValue <= 0
+                                ? 0
+                                : 100 - rightKnobPercentSize - rightKnobPercent
                         }%`;
                         setValueInPercent((prev) => [
-                            percentValue <= 0 ? 0 : 100,
+                            percentValue <= 0
+                                ? 0
+                                : rightKnobPercent === 0
+                                ? 100
+                                : 100 - rightKnobPercentSize - rightKnobPercent,
                             prev[1],
-                        ]);
-                        handleOnChange([
-                            percentValue <= 0 ? 0 : 100,
-                            valueInPercent[1],
                         ]);
                         leftDvider.current.style.width = `${
                             percentValue <= 0 ? 0 : trackWidth
@@ -185,51 +215,27 @@ export const LRangeSliderComponent = ({
                         ((clientX - trackOffsetLeft + rightKnobWidth / 2) *
                             100) /
                             trackWidth;
-
                     if (
-                        percentValue + rightKnobPercentSize <= 100 &&
+                        percentValue + rightKnobPercentSize <=
+                            100 - leftKnobPercent &&
                         percentValue >= 0
                     ) {
                         if (
-                            100 - percentValue - rightKnobPercentSize >=
+                            100 - percentValue - leftKnobPercentSize >=
                                 leftKnobPercent ||
                             100 - percentValue >= rightKnobPercent
                         ) {
+                            rightKnob.current.style.zIndex = '10';
+                            leftKnob.current.style.zIndex = '9';
                             rightKnob.current.style.right = `${percentValue}%`;
                             setValueInPercent((prev) => [
                                 prev[0],
                                 100 - percentValue - rightKnobPercentSize,
                             ]);
 
-                            handleOnChange([
-                                valueInPercent[0],
-                                100 - percentValue - rightKnobPercentSize,
-                            ]);
-
                             rightDvider.current.style.width = `${
                                 trackWidth - (clientX - trackOffsetLeft)
                             }px`;
-                        } else {
-                            rightKnob.current.style.right = `${
-                                100 - leftKnobPercent - rightKnobPercentSize
-                            }%`;
-                            setValueInPercent((prev) => [
-                                prev[0],
-                                100 -
-                                    (100 -
-                                        leftKnobPercent +
-                                        rightKnobPercentSize),
-                            ]);
-
-                            handleOnChange([
-                                valueInPercent[0],
-                                100 -
-                                    (100 -
-                                        leftKnobPercent +
-                                        rightKnobPercentSize),
-                            ]);
-
-                            rightDvider.current.style.width = `${trackWidth}px`;
                         }
                     } else if (
                         100 - percentValue - rightKnobPercentSize >=
@@ -242,10 +248,7 @@ export const LRangeSliderComponent = ({
                             prev[0],
                             percentValue <= 0 ? 100 : 0,
                         ]);
-                        handleOnChange([
-                            valueInPercent[0],
-                            percentValue <= 0 ? 100 : 0,
-                        ]);
+
                         rightDvider.current.style.width = `${
                             percentValue <= 0 ? 0 : trackWidth
                         }px`;
@@ -278,5 +281,7 @@ export const LRangeSliderComponent = ({
         rightKnob,
         handleStartMove,
         valueInPercent,
+        leftKnobMoving,
+        rightKnobMoving,
     };
 };
